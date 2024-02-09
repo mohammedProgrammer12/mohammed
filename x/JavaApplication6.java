04ce51c8e29e59e7400428750f4ad782cbecaaf1
mohammedProgrammer12 <157875858+mohammedProgrammer12@users.noreply.github.com>
2024-02-09 23:46:54 +0300

    Add files via upload

---
+++ x/JavaApplication6.java
@@ -0,0 +1,14 @@
+
+package javaapplication6;
+
+
+public class JavaApplication6 {
+
+    
+    public static void main(String[] args) {
+       int a=60,b=40;
+       int c=a+b;
+        System.out.println("c"+c);
+    }
+    
+}
